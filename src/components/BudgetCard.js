501c991a98c57e30638d6023af051197d851f0d9
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-19 13:56:03 +0430

    debug currency formatter in budget card

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -23,7 +23,7 @@ const BudgetCard = ({data}) => {
             <Card.Body>
                 <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
                     <div className="ms-2">{name}</div>
-                    <div className="d-flex align-items-baseline">{currencyFormatter(amount)}<span className="text-muted fs-6 ms-1">/{currencyFormatter(max)}</span></div>
+                    <div className="d-flex align-items-baseline">{currencyFormatter.format(amount)}<span className="text-muted fs-6 ms-1">/{currencyFormatter.format(max)}</span></div>
                 </Card.Title>
                 <ProgressBar className="rounded-pill" variant={getProgressBarVariant(amount, max)} min={0} max={max} now={amount}/>
                 <Stack direction="horizontal" gap="2" className="mt-4">
