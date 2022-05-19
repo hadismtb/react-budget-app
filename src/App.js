bca266148f247a436844f9c53ddc94ff0c7f857d
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-19 12:50:07 +0430

    import cards component to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {Container} from "react-bootstrap";
 
 //COMPONENTS
 import Header from "./components/Header";
+import Cards from "./components/Cards";
 
 
 function App() {
   return (
    <Container>
      <Header/>
+     <Cards/>
    </Container>
   );
 }
