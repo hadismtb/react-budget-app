b0ee89e72ee597ba5d28742e3e2943a13d38139d
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-19 17:04:11 +0430

    set a dynamically background-color for budget card

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -19,11 +19,11 @@ const BudgetCard = ({data}) => {
     }
 
     return (
-        <Card>
+        <Card className={amount > max ? `bg-danger bg-opacity-10` : "bg-light"}>
             <Card.Body>
                 <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
                     <div className="ms-2">{name}</div>
-                    <div className="d-flex align-items-baseline">{formatNumber(max)}<span className="text-muted fs-6 ms-1">/{formatNumber(max)}</span></div>
+                    <div className="d-flex align-items-baseline">{formatNumber(amount)}<span className="text-muted fs-6 ms-1">/{formatNumber(max)}</span></div>
                 </Card.Title>
                 <ProgressBar className="rounded-pill" variant={getProgressBarVariant(amount, max)} min={0} max={max} now={amount}/>
                 <Stack direction="horizontal" gap="2" className="mt-4 justify-content-end">
