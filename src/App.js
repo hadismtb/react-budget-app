bc06ce82deae7169e8c5c98f0ddfdd739a37424a
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-19 19:01:38 +0430

    wrap our app into budgetsProvider context

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import {Container} from "react-bootstrap";
 import Header from "./components/Header";
 import Cards from "./components/Cards";
 
+//CONTEXT
+import { BudgetsProvider} from "./context/BudgetContext";
 
 function App() {
   return (
-   <Container>
-     <Header/>
-     <Cards/>
-   </Container>
+   <BudgetsProvider>
+       <Container>
+           <Header/>
+           <Cards/>
+       </Container>
+   </BudgetsProvider>
   );
 }
 
