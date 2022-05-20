7d85e0d7983c613a1e89f6f2e6193c47c2ae6a0a
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-20 12:11:48 +0430

    get handle show add budget modal function as a props and use it for handle click on add budget button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Button, Stack} from "react-bootstrap";
 
-const Header = () => {
+const Header = ({handleAddBudgetModal}) => {
     return (
         <Stack direction="horizontal" gap="2" className="my-4">
             <h1 className="ms-auto">بودجه‌ها</h1>
-            <Button variant="primary">افزودن بودجه</Button>
+            <Button variant="primary" onClick={handleAddBudgetModal}>افزودن بودجه</Button>
             <Button variant="outline-primary">افزودن هزینه</Button>
         </Stack>
     );
