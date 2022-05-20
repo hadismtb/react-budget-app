8dbee96bcb83e87ce72fc8be302366906434781c
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-20 13:47:04 +0430

    get add expense handle show from props and set it as add expense button click handler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Button, Stack} from "react-bootstrap";
 
-const Header = ({handleAddBudgetModal}) => {
+const Header = ({handleAddBudgetModal, handleAddExpenseModal}) => {
     return (
         <Stack direction="horizontal" gap="2" className="my-4">
             <h1 className="ms-auto">بودجه‌ها</h1>
             <Button variant="primary" onClick={handleAddBudgetModal}>افزودن بودجه</Button>
-            <Button variant="outline-primary">افزودن هزینه</Button>
+            <Button variant="outline-primary" onClick={handleAddExpenseModal}>افزودن هزینه</Button>
         </Stack>
     );
 };
