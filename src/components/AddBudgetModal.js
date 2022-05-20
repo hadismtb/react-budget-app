91088e3a3ec0d9a5f57c5776a4abcfb41ae66474
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-20 10:52:09 +0430

    create base of add budget modal

---
+++ src/components/AddBudgetModal.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Button, Form, Modal} from "react-bootstrap";
+
+const AddBudgetModal = ({ show, handleClose }) => {
+    const handleSubmit = event => {
+        event.preventDefault();
+
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Form onSubmit={handleSubmit}>
+                <Modal.Header closeButton>
+                    <Modal.Title>بودجه جدید</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="mb-3" controlId="name">
+                        <Form.Label>عنوان</Form.Label>
+                        <Form.Control type="text" required/>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="max">
+                        <Form.Label>حداکثر مخارج</Form.Label>
+                        <Form.Control type="number"min={0} step={.01} required/>
+                    </Form.Group>
+                    <div className="d-flex justify-content-end">
+                        <Button variant="primary" type="submit">افزودن</Button>
+                    </div>
+                </Modal.Body>
+            </Form>
+        </Modal>
+    );
+};
+
+export default AddBudgetModal;
