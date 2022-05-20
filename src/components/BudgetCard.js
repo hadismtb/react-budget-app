37683e641bf095f3fe198fdb50e9748323e744f4
Hadise Matloob <hadisematloob@yahoo.com>
2022-05-21 00:54:46 +0430

    doing some change on budget card because  of showing a good result for our uncategorized expense

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -27,9 +27,9 @@ const BudgetCard = ({data, openAddExpense}) => {
             <Card.Body>
                 <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
                     <div className="ms-2">{name}</div>
-                    <div className="d-flex align-items-baseline">{formatNumber(amount)}<span className="text-muted fs-6 ms-1">/{formatNumber(max)}</span></div>
+                    <div className="d-flex align-items-baseline">{formatNumber(amount)}{max && <span className="text-muted fs-6 ms-1">/{formatNumber(max)}</span>}</div>
                 </Card.Title>
-                <ProgressBar className="rounded-pill" variant={getProgressBarVariant(amount, max)} min={0} max={max} now={amount}/>
+                {max && <ProgressBar className="rounded-pill" variant={getProgressBarVariant(amount, max)} min={0} max={max} now={amount}/>}
                 <Stack direction="horizontal" gap="2" className="mt-4 justify-content-end">
                     <Button variant="outline-primary" onClick={() => openAddExpense(id)}>افزودن هزینه</Button>
                     <Button variant="outline-secondary">مشاهده هزینه‌ها</Button>
